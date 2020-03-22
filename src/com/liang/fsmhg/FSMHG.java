765c931fcbae6c169392f08d240fadaf12df43ce
liliang <liliang@liliangdeMacBook-Pro.local>
2020-03-22 21:38:36 +0800

    begin fsmgh

--- src/com/liang/fsmhg/FSMHG.java
+++ src/com/liang/fsmhg/FSMHG.java
@@ -1,12 +1,25 @@
 package com.liang.fsmhg;
 
+import com.liang.fsmhg.graph.LabeledGraph;
 import com.liang.fsmhg.graph.Snapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FSMHG {
 
-    public void partition(List<Snapshot> snapshots) {
+    private boolean storePatterns;
 
+    public PatternTree mine(List<? extends LabeledGraph> graphs, double minSupport, double similarity, int maxEdgeNum) {
+        List edgePatterns = singleEdgePatterns(graphs);
+        for (Object o : edgePatterns) {
+
+        }
+        return null;
     }
+
+    private List singleEdgePatterns(List<? extends LabeledGraph> graphs) {
+        return null;
+    }
+
 }
