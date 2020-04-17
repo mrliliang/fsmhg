72edf5707dcab570119b1e2344d0af82571e2de0
liliang <liliang@liliangdeMacBook-Pro.local>
2020-04-17 20:45:33 +0800

    test numeration time

--- src/main/java/com/liang/fsmhg/Embedding.java
+++ src/main/java/com/liang/fsmhg/Embedding.java
@@ -3,16 +3,23 @@ package com.liang.fsmhg;
 import com.liang.fsmhg.graph.LabeledVertex;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Embedding {
 
     private Embedding parent;
     private LabeledVertex v;
+    private int size;
 
     public Embedding(LabeledVertex v, Embedding parent) {
         this.v = v;
         this.parent = parent;
+        if (parent == null) {
+            size = 1;
+        } else {
+            size = parent.size + 1;
+        }
     }
 
     public LabeledVertex vertex() {
@@ -20,7 +27,10 @@ public class Embedding {
     }
 
     public List<LabeledVertex> vertices() {
-        return vertices(1);
+        ArrayList<LabeledVertex> list = new ArrayList<>(size);
+//        return vertices(1);
+        vertices(list);
+        return list;
     }
 
     private List<LabeledVertex> vertices(int count) {
@@ -32,6 +42,19 @@ public class Embedding {
         List<LabeledVertex> vertices = this.parent.vertices(count + 1);
         vertices.add(v);
         return vertices;
+    }
+
+    private void vertices(List<LabeledVertex> list) {
+        if (this.parent == null) {
+            list.add(v);
+            return;
+        }
+        parent.vertices(list);
+        list.add(v);
+    }
+
+    public int size() {
+        return size;
     }
 
 }
