cdac694de7c955571fa1fff6058fa8c91092853e
liliang <630425651@qq.com>
2020-04-20 15:38:31 +0800

    change fsmgh.sh

--- fsmgh.sh
+++ fsmgh.sh
@@ -1,1 +1,10 @@
-java -jar target/fsmhg.jar -d test -s 0.3 -o out/test_result
+java -jar target/fsmhg.jar -d test -s 0.5 -o out/test_result_5000
+
+java -jar target/fsmhg.jar -d test -s 0.3 -o out/test_result_3000
+
+java -jar target/fsmhg.jar -d test -s 0.1 -o out/test_result_1000
+
+java -jar target/fsmhg.jar -d test -s 0.06 -o out/test_result_600
+
+java -jar target/fsmhg.jar -d test -s 0.04 -o out/test_result_400
+
